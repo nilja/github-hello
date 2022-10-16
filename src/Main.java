135ae1bc8eb8de5da7f22a6461e34a1cf4391fae
Nilesh <nilja05@gmail.com>
2022-10-16 10:21:41 +0530

    Added Java docs on the main method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+    /**
+     * Method to print Hello world!
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
